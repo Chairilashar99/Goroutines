4c1ba7849697d0e92b764b16cbbcc10241c5c136
chairil ashar <asharchairil4@gmail.com>
2023-06-19 13:18:55 +0700

    select ch

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajargolanggoroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -84,6 +85,51 @@ func TestBufferedChannel(t *testing.T) {
 
 	time.Sleep(3 * time.Second)
 	fmt.Println("Selesai Mengirim Data ke Channel")
+}
+
+//Range Channel untuk data yg banyak dan hanya 1 channel
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+	fmt.Println("Selesai")
+
+}
+
+//Select Channel
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
 }
 
 //Catatan
